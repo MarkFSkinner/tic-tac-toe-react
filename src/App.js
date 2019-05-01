a19740f7b3dd967d3063d39041975fa565856e83
Mark Skinner <markskinner850@gmail.com>
2019-05-01 15:54:06 -0400

    Add state to app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,43 @@ import Board from './components/Board';
 import Controls from './components/Controls';
 import Menu from './components/Menu';
 
-function App() {
-  return (
-    <div className="container text-center">
-      <CurrentPlayer />
-      <Board />
-      <Controls />
-      <Menu />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    availableMoves: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    playerOneMoves: [],
+    playerTwoMoves: [],
+    computerMoves: [],
+    winCombos: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ],
+    gameStarted: false,
+    numPlayers: undefined,
+    playerOneToken: undefined,
+    playerTwoToken: undefined,
+    computerToken: undefined,
+    difficultyLevel: undefined,
+    currentPlayer: 'playerOne',
+    win: undefined
+  }
+
+
+  render() {
+    return (
+      <div className='container text-center'>
+        <CurrentPlayer />
+        <Board />
+        <Controls />
+        <Menu />
+      </div>
+    );
+  }
 }
 
 export default App;
